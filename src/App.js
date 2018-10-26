1dba2aa6842451e5cbae9daba2df3972f9817b6e
Anuj Shrestha <anujshrestha@lftechnology.com>
2018-10-26 11:24:54 +0545

    Add planes

--- src/App.js
+++ src/App.js
@@ -8,66 +8,27 @@ class App extends Component {
   render() {
     return (
       <Flight interactive ref={flight => (this.flight = flight)}>
-        <Flight.Frame duration={300} source interactive>
+        <Flight.Frame duration={3000} source interactive>
           <div className="keyframe">
             <Rect
               name="head-1"
               radius={5}
               style={{
-                backgroundColor: '#95A2AA',
-                left: 30 - 20,
+                right: -60,
                 top: 20,
                 width: 50,
                 height: 10,
               }}
-            />
+            >
+              <ion-icon name="ios-airplane"
+                style={{
+                  display: 'inline-block',
+                  transform: 'rotate(180deg)'
+                }}
 
-            <Rect
-              name="head-2"
-              radius={5}
-              style={{
-                backgroundColor: '#E6ECF0',
-                left: 120 - 15,
-                top: 20,
-                width: 120,
-                height: 10,
-              }}
-            />
-            <Rect
-              name="head-4"
-              radius={5}
-              style={{
-                backgroundColor: '#E6ECF0',
-                right: 15,
-                top: 20,
-                width: 30,
-                height: 10,
-              }}
-            />
+              ></ion-icon>
 
-            <Rect
-              name="img-bg"
-              radius={5}
-              style={{
-                backgroundColor: '#E6ECF0',
-                left: 80 - 30,
-                top: 120,
-                width: 90,
-                height: 90,
-              }}
-            />
-
-            <Rect
-              name="img-main"
-              radius={5}
-              style={{
-                backgroundColor: '#4DA1FF',
-                left: 50 - 15,
-                top: 100,
-                width: 90,
-                height: 90,
-              }}
-            />
+            </Rect>
 
             <div
               id="brace-1"
@@ -79,7 +40,7 @@ class App extends Component {
                 fontSize: 30,
               }}
             >
-              {'{'}
+              <i class="fa fa-fighter-jet" aria-hidden="true"></i>
             </div>
             <div
               id="brace-2"
@@ -97,67 +58,26 @@ class App extends Component {
 
         </Flight.Frame>
 
-        <Flight.Frame duration={300}>
+        <Flight.Frame duration={3000}>
           <div className="keyframe">
             <Rect
               name="head-1"
               radius={5}
               style={{
-                backgroundColor: '#95A2AA',
                 left: -60,
                 top: 20,
                 width: 50,
                 height: 10,
               }}
-            />
+            >
+              <ion-icon name="ios-airplane"
+                style={{
+                  display: 'inline-block',
+                  transform: 'rotate(90deg)'
+                }}
+              ></ion-icon>
 
-            <Rect
-              name="head-2"
-              radius={5}
-              style={{
-                backgroundColor: '#E6ECF0',
-                left: 120 - 15,
-                top: -15,
-                width: 120,
-                height: 10,
-              }}
-            />
-
-            <Rect
-              name="head-4"
-              radius={5}
-              style={{
-                backgroundColor: '#E6ECF0',
-                right: -80,
-                top: 20,
-                width: 30,
-                height: 10,
-              }}
-            />
-
-            <Rect
-              name="img-bg"
-              radius={5}
-              style={{
-                backgroundColor: '#E6ECF0',
-                left: 80 - 15,
-                top: 360,
-                width: 90,
-                height: 90,
-              }}
-            />
-
-            <Rect
-              name="img-main"
-              radius={5}
-              style={{
-                backgroundColor: '#4DA1FF',
-                left: 50 - 15,
-                top: 120,
-                width: 60,
-                height: 60,
-              }}
-            />
+            </Rect>>
 
             <div
               id="brace-1"
@@ -165,11 +85,11 @@ class App extends Component {
               style={{
                 color: '#95A2AA',
                 top: 30,
-                left: 20,
+                left: 1000,
                 fontSize: 30,
               }}
             >
-              {'{'}
+              <i class="fa fa-fighter-jet" aria-hidden="true"></i>
             </div>
             <div
               id="brace-2"
